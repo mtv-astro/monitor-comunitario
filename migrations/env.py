6eb330e49c54d0f1c702a92d38b2a1ae5f803d86
mtv-astro <mtv-astro@proton.me>
2026-06-17 11:30:16 -0300

    chore(migrations): reorder imports in env.py to satisfy lint

--- migrations/env.py
+++ migrations/env.py
@@ -1,27 +1,44 @@
 from logging.config import fileConfig
 
 from alembic import context
-from monitor_comunitario.core.config import get_settings
-from monitor_comunitario.db.models import Base
 from sqlalchemy import engine_from_config, pool
 
+from monitor_comunitario.core.config import get_settings
+from monitor_comunitario.db.models import Base
+
+# this is the Alembic Config object, which provides access
+# to the values within the .ini file in use.
 config = context.config
 
+# Interpret the config file for Python logging.
+# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
+# add your model's MetaData object here
+# for 'autogenerate' support, refer to the Alembic documentation.
 target_metadata = Base.metadata
 
 
 def run_migrations_offline() -> None:
-    """Run migrations in offline mode."""
+    """Run migrations in 'offline' mode.
+
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+
+    Calls to context.execute() here emit the given string to the
+    script output.
+
+    """
     settings = get_settings()
+    url = settings.database_url
     context.configure(
-        url=settings.database_url,
+        url=url,
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
-        compare_type=True,
     )
 
     with context.begin_transaction():
@@ -29,9 +46,11 @@ def run_migrations_offline() -> None:
 
 
 def run_migrations_online() -> None:
-    """Run migrations in online mode."""
+    """Run migrations in 'online' mode."""
     settings = get_settings()
-    configuration = config.get_section(config.config_ini_section) or {}
+    configuration = config.get_section(config.config_ini_section)
+    if configuration is None:
+        configuration = {}
     configuration["sqlalchemy.url"] = settings.database_url
 
     connectable = engine_from_config(
@@ -41,11 +60,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection,
-            target_metadata=target_metadata,
-            compare_type=True,
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
